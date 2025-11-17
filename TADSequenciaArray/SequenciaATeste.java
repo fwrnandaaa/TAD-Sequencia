38383cbc236009478c3fbdd6bc26617163918d8d
Fernanda Dantas do Nascimento <110307073+fwrnandaaa@users.noreply.github.com>
2025-11-17 14:12:41 +0000

    Implementações

--- TADSequenciaArray/SequenciaATeste.java
+++ TADSequenciaArray/SequenciaATeste.java
@@ -1,7 +1,24 @@
 public class SequenciaATeste {
     public static void main(String[] args){
         TadSequenciaArray s = new TadSequenciaArray(5);
+        s.insertFirst(2);
         s.insertFirst(1);
+        System.out.println("Elemento no rank 1: "+ s.elemAtRank(1));
+        System.out.println("------------- insert at rank -------------");
+        s.insertAtRank(1, 3);
+         s.insertAtRank(3, 4);
+        s.verSequencia();
+        System.out.println("------------- replace at rank -------------");
+        s.replaceAtRank(2, 5);
+        s.verSequencia();
+        System.out.println("------------- remove at rank -------------");
+        s.removeAtRank(0);
+        s.verSequencia();
+        System.out.println("------------- replace element -------------");
+        s.replaceElement(5, 200);
+        s.verSequencia();
+        System.out.println("------------- swap elements -------------");
+        s.swapElements(3,4);
         s.verSequencia();
     }
 }
--- TADSequenciaArray/TadSequenciaArray.java
+++ TADSequenciaArray/TadSequenciaArray.java
@@ -17,6 +17,16 @@ public class TadSequenciaArray{
         }
         return false;
     }
+    public void aumenta_capacidade(){
+        if(size >= capacidade){
+            capacidade = capacidade*2;
+            Object[] aux = new Object[capacidade];
+            for(int i =0; i<size; i++){
+                aux[i] = tad_sequencia[i];
+            }
+            tad_sequencia = aux;
+        }
+    }
     public Object elemAtRank(int r){
         if(isEmpty()){
             throw new TadSequenciaExcecao("O array está vazio.");
@@ -24,7 +34,7 @@ public class TadSequenciaArray{
         if(r >=size){
             throw new TadSequenciaExcecao("O rank informado é maior que o tamanho do array.");
         }
-        return tad_sequencia[r];
+        return tad_sequencia[r-1];
     }
     public void verSequencia(){
         for(int i=0;i<size;i++){
@@ -36,11 +46,121 @@ public class TadSequenciaArray{
             tad_sequencia[0]=O;
         }
         else{
-            for(int i =size; i>=0;i--){
+            aumenta_capacidade();
+            for(int i =size; i>0;i--){
                 tad_sequencia[i] = tad_sequencia[i-1];
             }
         }
         tad_sequencia[0]=O;
         size++;
+    }
+    public void replaceAtRank(int i, Object O){
+        if(i >size){
+            throw new TadSequenciaExcecao("Rank informado não existe.");
+        }
+        tad_sequencia[i] = O;
+    }
+    public void insertAtRank(int r, Object O){
+        if(r >size || r<0){
+            throw new TadSequenciaExcecao("O rank informado não existe.");
+        }
+        aumenta_capacidade();
+        for(int i = size; i>r; i--){
+            tad_sequencia[i] = tad_sequencia[i-1];
+        }
+        tad_sequencia[r]= O;
+        size++;
+    }
+    public void removeAtRank(int r){
+        if(r < 0|| r >= size){
+            throw new TadSequenciaExcecao("Rank inválido.");
+        }
+        if(r==size){
+            tad_sequencia[size-1] = null;
+            size--;
+            return;
+        }
+        for(int i = r; i<size-1; i++){
+            tad_sequencia[i] = tad_sequencia[i+1];
+        }
+        size--;
+    }
+    public Object first(){
+        if(size==0){
+            throw new TadSequenciaExcecao("Lista vazia.");
+        }
+        return tad_sequencia[0];
+    }
+    public Object last(){
+        if(size==0){
+            throw new TadSequenciaExcecao("Lista vazia.");
+        }
+        return tad_sequencia[size-1];
+    }
+    public Object before(Object n){
+        if(size==0){
+            throw new TadSequenciaExcecao("O array está vazio.");
+        }
+        if(tad_sequencia[0].equals(n)){
+            throw new TadSequenciaExcecao("Objeto informado é o primeiro elemento do array.");
+        }
+        for(int i=1; i<size;i++){
+            if(tad_sequencia[i].equals(n)){
+                return tad_sequencia[i-1];
+            }
+        }
+        throw new TadSequenciaExcecao("Objeto informado não foi encontrado na lista.");
+    }
+    public Object after(Object n){
+        if(size==0){
+            throw new TadSequenciaExcecao("O array está vazio.");
+        }
+        if(tad_sequencia[size-1].equals(n)){
+            throw new TadSequenciaExcecao("Objeto informado é o ultimo elemento do array.");
+        }
+        for(int i=0; i<size-1;i++){
+            if(tad_sequencia[i].equals(n)){
+                return tad_sequencia[i+1];
+            }
+        }
+        throw new TadSequenciaExcecao("Objeto informado não foi encontrado na lista.");
+    }
+    public void replaceElement(Object N, Object O){
+        if(size==0){
+            throw new TadSequenciaExcecao("O Array está vazio.");
+        }
+        for(int i =0; i<size; i++){
+            if(tad_sequencia[i].equals(N)){
+                tad_sequencia[i]=O;
+                return;
+            }
+        }
+        throw new TadSequenciaExcecao("O Objecto informado não foi encontrado");
+    }
+    public void swapElements(Object N, Object O){
+        if(size==0){
+            throw new TadSequenciaExcecao("A lista está vazia");
+        }
+        int indiceN =-1;
+        int indiceO=-1;
+        for(int i = 0; i<size;i++){
+            if(tad_sequencia[i].equals(N) && indiceN == -1){
+                indiceN=i;
+            }
+            if(tad_sequencia[i].equals(O) && indiceO == -1){
+                indiceO=i;
+            }
+        }
+        if(indiceN ==-1 || indiceO ==-1){
+            throw new TadSequenciaExcecao("Um dos objetos informados não existe no array.");
+        }
+        tad_sequencia[indiceN] = O;
+        tad_sequencia[indiceO] = N;
+    }
+    public void insertBefore(Object N, Object O){
+            if(size==0){
+                throw new TadSequenciaExcecao("O Array está vazio.");
+            }
+
     }
 }
